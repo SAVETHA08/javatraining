ec32c0f12cabd2b7ba4e71d17ccefc8200549790
SAVETHA08 <saveuma1999@gmail.com>
2021-11-15 12:48:54 +0530

    initial project commit

---
+++ Oddeven/src/Oddeven/OddevenClass.java
@@ -0,0 +1,13 @@
+package Oddeven;
+public class OddevenClass {
+    static int a=10;
+	public static void main(String[] args) {
+		if(a % 2 == 0) {
+		System.out.println("even");
+		}
+		else {
+			System.out.println("odd");
+		}
+	}
+
+}
